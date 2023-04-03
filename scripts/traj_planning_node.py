5c3ffc2516aeac5a6272a304c790f7947585327d
Zixu <zixuz@princeton.edu>
2023-04-03 12:39:11 -0400

    add setting to avoid memory overflow

--- scripts/traj_planning_node.py
+++ scripts/traj_planning_node.py
@@ -8,6 +8,8 @@ import sys, os
 
 
 if __name__ == '__main__':
+    # Safe guard for GPU memory
+    os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "20" 
     rospy.init_node('traj_planning_node')
     rospy.loginfo("Start trajectory planning node")
 
