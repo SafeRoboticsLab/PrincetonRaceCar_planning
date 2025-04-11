fafb59e5c12dac061aa3a18a23c219d8c5341b5d
rzhou57 <69701858+rzhou57@users.noreply.github.com>
2025-04-11 02:41:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,22 +8,31 @@ setup(
     name=package_name,
     version='0.0.1',
     packages=['ILQR', 'utils'],
-    package_dir={'': 'scripts'},  # Your source files are in scripts/
+    package_dir={'': 'scripts'},
     data_files=[
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),      # launch files
-        ('share/' + package_name + '/configs', glob('configs/*.yaml')),         # config files
-        ('share/' + package_name, ['package.xml']),                             # package manifest
+        # Install package.xml
+        ('share/' + package_name, ['package.xml']),
+        
+        # Install launch files (both .launch and .launch.py)
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch*')),
+        
+        # Install config YAMLs and model files
+        (os.path.join('share', package_name, 'configs'), glob('configs/*.yaml') + glob('configs/*.sav')),
+        
+        # Register resource index
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='',
-    maintainer_email='',
-    description='iLQR-based planner for autonomous racing',
+    maintainer='YOUR_NAME',
+    maintainer_email='YOUR_EMAIL@domain.com',
+    description='iLQR-based planner for autonomous racing cars with dynamic/static obstacle avoidance and PWM control.',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'traj_planning_node = scripts.traj_planning_node:main',  # entry point to run your node
+            # ROS 2 launchable script entry
+            'traj_planning_node = scripts.traj_planning_node:main',
         ],
     },
 )
