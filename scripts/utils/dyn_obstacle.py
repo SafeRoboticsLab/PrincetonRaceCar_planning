500343ef6872fe62eb304fa16dfc9ff14088bdee
rzhou57 <69701858+rzhou57@users.noreply.github.com>
2025-04-11 01:26:02 -0400

    Update dyn_obstacle.py

--- scripts/utils/dyn_obstacle.py
+++ scripts/utils/dyn_obstacle.py
@@ -1,57 +1,51 @@
 import numpy as np
-import rospy
 from racecar_obs_detection.srv import GetFRS, GetFRSResponse
 from visualization_msgs.msg import Marker, MarkerArray
+from builtin_interfaces.msg import Duration
 
-
-def frs_to_obstacle(frs_respond: GetFRSResponse)->list:
-    '''
-    This function converts the response from the FRS service to a list of obstacles
-    Parameters:
-        frs_respond: GetFRSResponse, the response from the FRS service
-    Returns:
-        obstacles_list: list of obstacles that can be used by ILQR
-    '''
+def frs_to_obstacle(frs_response: GetFRSResponse) -> list:
+    """
+    Convert FRS service response into a list of obstacles usable by ILQR.
+    Each obstacle is a list of 2D polygons (one per timestep).
+    """
     obstacles_list = []
-    for frs in frs_respond.FRS: # A list of SetArray
+    for frs in frs_response.FRS:  # List of SetArray
         vertices_list = []
-        for frs_t in frs.set_list: # A list of polygon in a setarry
-            polygon = []
-            for points in frs_t.points:
-                polygon.append([points.x, points.y])
+        for frs_t in frs.set_list:  # List of polygons over time
+            polygon = [[p.x, p.y] for p in frs_t.points]
             vertices_list.append(np.array(polygon))
         obstacles_list.append(vertices_list)
     return obstacles_list
 
-def frs_to_msg(frs_respond: GetFRSResponse)->MarkerArray:
-    '''
-    This function converts the response from the FRS service to a MarkerArray for visualization
-    '''
+def frs_to_msg(frs_response: GetFRSResponse) -> MarkerArray:
+    """
+    Convert FRS data to a MarkerArray for visualization in RViz.
+    Each polygon becomes a LINE_STRIP marker.
+    """
     marker_array = MarkerArray()
-    
-    if frs_respond.FRS is None:
+    if not frs_response.FRS:
         return marker_array
-    
-    for i, frs in enumerate(frs_respond.FRS): # A list of SetArray
-        for t, frs_t in enumerate(frs.set_list): # A list of polygon in a setarry
+
+    for i, frs in enumerate(frs_response.FRS):
+        for t, frs_t in enumerate(frs.set_list):
             marker = Marker()
             marker.header.frame_id = "map"
-            marker.ns = "frs_" + str(i)
+            marker.ns = f"frs_{i}"
             marker.id = t
-            marker.action = 0
-            marker.type = 4 # line strip
-            
-            marker.color.r = 204.0/255.0
-            marker.color.g = 102.0/255.0
-            marker.color.b = 0.0/255.0
+            marker.action = Marker.ADD
+            marker.type = Marker.LINE_STRIP
+
+            marker.color.r = 204.0 / 255.0
+            marker.color.g = 102.0 / 255.0
+            marker.color.b = 0.0
             marker.color.a = 0.5
-            
-            marker.points = frs_t.points
-            marker.points.append(frs_t.points[0])
-            
+
+            # Add points and loop back to first point
+            marker.points = frs_t.points + [frs_t.points[0]]
+
             marker.scale.x = 0.01
-            
-            marker.lifetime = rospy.Duration(0.2)
-            
+            marker.lifetime = Duration(sec=0, nanosec=200_000_000)  # 0.2 seconds
+
             marker_array.markers.append(marker)
+
     return marker_array
